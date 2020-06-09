23d64a4d3921b462f73a837084c4c2a2f6b4ae67
manong <manong>
2020-06-09 10:49:00 +0800

    修复了别的设备拉取项目无法运行

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -70,7 +70,7 @@ class _HomeState extends State<Home> {
               onPressed: () {
                 Navigator.push( context, MaterialPageRoute( builder: (context) => FlutterFileSelector(
                       isScreen: true,
-                      fileTypeEnd: [".pdf", ".doc", ".docx"],
+                      fileTypeEnd: [".pdf", ".doc", ".docx","xls","xlsx"],
                     ), ), ).then( (value) => setState( () => v = value),
                 );
               },
